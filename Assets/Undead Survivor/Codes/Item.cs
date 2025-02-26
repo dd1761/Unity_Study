36813b372eabaa0bc0608e1f1fc3abd6136ae0a5
KIMDONGBEEN <dd1761@naver.com>
2025-02-26 17:46:55 +0900

    123

---
+++ Assets/Undead Survivor/Codes/Item.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    public ItemData data;
+    public int level;
+    public Weapon weapon;
+
+    Image icon;
+    Text textLevel;
+
+    void Awake()
+    {
+        icon = GetComponentsInChildren<Image>()[1];
+        icon.sprite = data.itemIcon;
+
+        Text[] texts = GetComponentsInChildren<Text>();
+        textLevel = texts[0];
+    }
+
+    void LateUpdate()
+    {
+        textLevel.text = "Lv." + (level + 1);
+    }
+}
